6862806640a121e591f724140d3460bbfcdbeb45
Jonas <joda12@elev.techcollege.dk>
2025-12-03 16:01:53 +0100

    Authenticate role added

--- my-api/src/routes/authRoutes.ts
+++ my-api/src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { authenticateToken } from "../middleware/authenticateToken.js";
 import { getUserProfile } from "../controllers/authController.js";
+import { authorizeRole } from "../middleware/authorizeRole.js";
 
 const router = Router()
 
-router.get('/authenticate', authenticateToken, getUserProfile)
+router.get('/authenticate', authenticateToken, authorizeRole('ADMIN', 'USER'), getUserProfile)
 
 export { router as authRoutes }
